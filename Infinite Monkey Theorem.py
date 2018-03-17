f451377141a6558c9b3df73f511599e67b17f2d0
HelloYuiYui <enes35ihsan@gmail.com>
2018-03-17 13:49:08 +0000

    Add files via upload
    Reviewed and changed code added.

---
+++ Infinite Monkey Theorem.py
@@ -0,0 +1,44 @@
+# Created by EIA, March 2018.
+# I am a beginner developer. I am not very good at it but i enjoy to it.
+
+# Aim is to create the infinite monkey theorem again.
+
+#needed stuff imported.
+import random
+import string
+
+#alphabet created to pick random numbers and adding them to the letters variable.
+alp = string.ascii_lowercase
+
+#count variable to count letters.
+count = 0
+#string to find within the letters.
+quote = input("what word do you want to generate: ")
+#limitation to quote's lenght. it tooks a lot of time and consumes computer's energy.
+while len(quote) > 10:
+    print("we cannot take an input longer than 10 characters for the safety of the computer, and it lasts too long.")
+    quote = input("what word do you want to generate: ")
+print("waiting for computer to calculate...")
+print("")
+print("fun fact: the possibility to find your unique string is approximately 1/" + str((27**(len(quote)))))
+print("")
+letters = ""
+
+#generating a random letter and adding it into the letters variable.
+while not quote in letters:
+    randChar = random.choice(alp)
+    letters = (letters + randChar)
+    #checking that if the quote is found
+    if quote in letters:
+        print(quote + ": it took " + (str(count)) + " letters to find the word " + quote)
+        break
+    #if quote couldn't found yet.
+    else:
+        count += 1
+
+#printing results
+print("")
+print(letters)
+print("")
+print(count)
+input("Press enter to contuniune... ")
